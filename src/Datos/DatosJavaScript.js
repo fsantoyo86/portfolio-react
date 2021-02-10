78f870967121ddfa94f2c47d8eb54d2cfd51d5d1
FSantoyo125 <pacolin86@gmail.com>
2021-02-10 15:26:40 -0600

    javascript first commit

--- src/Datos/DatosJavaScript.js
+++ src/Datos/DatosJavaScript.js
@@ -1,8 +1,11 @@
 export const Data = [
   {
     id: 1,
-    title: "Sitio responsive",
+    title: "Quiz: test-física",
     overview:
-      "Proyecto ...",
+      "Cuestionario de física, ejemplo de como realizar un cuestionario en javascript, html y css.",
+    image: "./img/quiz.png",
+    sitio: "https://test-fisica.netlify.app/",
+    github: "https://github.com/fsantoyo86/test-fisica",
   },
 ];
