cab7d1008e341eb8e2925caa1214d106783466d4
FSantoyo125 <pacolin86@gmail.com>
2021-02-12 15:16:43 -0600

    todo list added commit

--- src/Datos/DatosJavaScript.js
+++ src/Datos/DatosJavaScript.js
@@ -8,4 +8,13 @@ export const Data = [
     sitio: "https://test-fisica.netlify.app/",
     github: "https://github.com/fsantoyo86/test-fisica",
   },
+  {
+    id: 2,
+    title: "To-Do list",
+    overview:
+      "Creación de una simple lista de tareas en javascript y almacenamiento local.",
+    image: "./img/todo.png",
+    sitio: "https://todo-list-zeta-pied.vercel.app/",
+    github: "https://github.com/fsantoyo86/todo-list",
+  },
 ];
