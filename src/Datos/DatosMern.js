b12bff0f44e3183b23b507484e131cfebb06a6d5
FSantoyo125 <pacolin86@gmail.com>
2021-02-15 10:27:09 -0600

    mern 2 commit

--- src/Datos/DatosMern.js
+++ src/Datos/DatosMern.js
@@ -2,9 +2,19 @@ export const Data = [
   {
     id: 1,
     title: "Registro Empleados CRUD",
-    overview: "Proyecto que permite realizar un registo de empleados utilizando todas las operaciones CRUD (crete, read, update, delete)",
+    overview:
+      "Proyecto que permite realizar un registo de empleados utilizando todas las operaciones CRUD (crete, read, update, delete)",
     image: "./img/empleados.png",
     sitio: "https://empleados-crud.vercel.app/",
     github: "https://github.com/fsantoyo86/empleados-CRUD",
+  },
+  {
+    id: 2,
+    title: "Notas CRUD",
+    overview:
+      "Proyecto que permite utilizar todas las operaciones CRUD (crete, read, update, delete) al crear notas",
+    image: "./img/nota.png",
+    sitio: "https://notas-fsantoyo.netlify.app/",
+    github: "https://github.com/fsantoyo86/notas-CRUD",
   },
 ];
